fc2875c4adb2f7ae67914874c68f7320b0e9ea3f
rodo-orsi <rodo.orsi@gmail.com>
2020-06-07 18:07:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,6 @@ else
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>PHP Application - AWS Elastic Beanstalk</title>
     <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster+Two" type="text/css">
     <link rel="icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
@@ -23,7 +22,6 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1> Manual upload... </h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
     </section>
