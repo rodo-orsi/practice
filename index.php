f50d6ac066256795902c5c940ef553ca69d91f2d
rodo-orsi <39849037+rodo-orsi@users.noreply.github.com>
2020-06-07 17:59:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1> Manual upload... </h1>
+        <h1> Deploy via GIT!! </h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
     </section>
